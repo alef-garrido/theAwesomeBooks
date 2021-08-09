41c11ad141819cbbe479e77d29e58d2448fde75b
Alef Garrido <oa.garrido11@gmail.com>
2021-08-09 16:51:32 -0500

    Add remove function

--- app.js
+++ app.js
@@ -32,20 +32,34 @@ function addNewBook() {
 function bookList() {
     bookListContainer = document.getElementById("books")
     books = getFromStorage();
-    books.map(book => {
-        bookContainer = document.createElement("li")
-        title_p_tag =  document.createElement("p")
+    books.map((book, index) => {
+        const bookContainer = document.createElement("li")
+        const title_p_tag =  document.createElement("p")
         title_p_tag.appendChild(document.createTextNode(book.title));
-
-        author_p_tag =  document.createElement("p")
+        bookContainer.id = index
+        const author_p_tag =  document.createElement("p")
         author_p_tag.appendChild(document.createTextNode(book.author));
 
-        remove_button_tag =  document.createElement("button")
+        const remove_button_tag =  document.createElement("button")
         remove_button_tag.appendChild(document.createTextNode("Remove"));
+        remove_button_tag.classList.add('delete-btn')
 
         bookContainer.append(title_p_tag, author_p_tag, remove_button_tag);
-
         bookListContainer.appendChild(bookContainer);
     })
-    
 }
+
+function removeBook(id) {
+  console.log(books)
+  books.splice(id, 1)
+  console.log(books)
+  saveToStorage(books)
+}
+
+const displayContainer = document.querySelector('#books')
+displayContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete-btn')) {
+    removeBook(e.target.parentNode.id)
+  }
+  bookList()
+})
