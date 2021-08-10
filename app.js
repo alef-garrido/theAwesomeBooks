0ebbda7ff45de52ef63df3a130a297238e384aa9
Maha Magdy <mahamagdy3@gmail.com>
2021-08-10 22:23:30 +0200

    create Book class

--- app.js
+++ app.js
@@ -3,39 +3,41 @@
 
 let books = [];
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
 function saveToStorage(arr) {
-  localStorage.setItem('bookList', JSON.stringify(arr));
+  localStorage.setItem("bookList", JSON.stringify(arr));
 }
 
 function getFromStorage() {
-  if (localStorage.getItem('bookList') === null) {
+  if (localStorage.getItem("bookList") === null) {
     books = [];
   } else {
-    books = JSON.parse(localStorage.getItem('bookList'));
+    books = JSON.parse(localStorage.getItem("bookList"));
   }
   return books;
 }
 
 function bookList() {
-  const bookListContainer = document.getElementById('books');
-  bookListContainer.innerHTML = '';
+  const bookListContainer = document.getElementById("books");
+  bookListContainer.innerHTML = "";
   books = getFromStorage();
   books.map((book, index) => {
-    const bookContainer = document.createElement('li');
-    const titlePTag = document.createElement('p');
+    const bookContainer = document.createElement("li");
+    const titlePTag = document.createElement("p");
     titlePTag.appendChild(document.createTextNode(book.title));
     bookContainer.id = index;
-    const authorPTag = document.createElement('p');
+    const authorPTag = document.createElement("p");
     authorPTag.appendChild(document.createTextNode(book.author));
 
-    const removeButtonTag = document.createElement('button');
-    removeButtonTag.appendChild(document.createTextNode('Remove'));
-    removeButtonTag.classList.add('delete-btn');
+    const removeButtonTag = document.createElement("button");
+    removeButtonTag.appendChild(document.createTextNode("Remove"));
+    removeButtonTag.classList.add("delete-btn");
 
     bookContainer.append(titlePTag, authorPTag, removeButtonTag);
     bookListContainer.appendChild(bookContainer);
@@ -43,8 +45,8 @@ function bookList() {
 }
 
 function addNewBook() {
-  const title = document.getElementById('bookTitle').value;
-  const author = document.getElementById('bookAuthor').value;
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
 
   const newBook = new Book(title, author);
 
@@ -58,9 +60,9 @@ function removeBook(id) {
   saveToStorage(books);
 }
 
-const displayContainer = document.querySelector('#books');
-displayContainer.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete-btn')) {
+const displayContainer = document.querySelector("#books");
+displayContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
     removeBook(e.target.parentNode.id);
   }
   bookList();
