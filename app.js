b9cd530a72e8cf57e4ea801ad739eb2871f129a0
Maha Magdy <mahamagdy3@gmail.com>
2021-08-10 23:27:09 +0200

    make changes on how to call methods inside collectionStorage and Bookcollection classes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable array-callback-return */
 
-let books = [];
+// let books = [];
 
 class Book {
   constructor(title, author) {
@@ -15,14 +15,15 @@ class Book {
 class CollectionStorage {
 
   static saveToStorage(arr) {
-    localStorage.setItem("BookCollecti", JSON.stringify(arr));
+    localStorage.setItem("BookCollection", JSON.stringify(arr));
   }
 
   static getFromStorage() {
-    if (localStorage.getItem("BookCollecti") === null) {
+    let books
+    if (localStorage.getItem("BookCollection") === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("BookCollecti"));
+      books = JSON.parse(localStorage.getItem("BookCollection"));
     }
     return books;
   }
@@ -30,52 +31,53 @@ class CollectionStorage {
 
 // Class of Book List
 class BookCollection {
-  constructor (book) {
-    
-  }
 
+  books = []
+ 
   static addNewBook() {
     const title = document.getElementById("bookTitle").value;
     const author = document.getElementById("bookAuthor").value;
   
     const newBook = new Book(title, author);
   
-    books.push(newBook);
+    this.books.push(newBook);
     CollectionStorage.saveToStorage(books);
-    BookCollecti();
+    this.bookList();
   }
 
   static removeBook(id) {
-    books.splice(id, 1);
+    this.books.splice(id, 1);
   }
-}
 
-function bookList() {
-  const BookCollectiContainer = document.getElementById("books");
-  BookCollectiContainer.innerHTML = "";
-  books = CollectionStorage.getFromStorage();
-  books.map((book, index) => {
-    const bookContainer = document.createElement("li");
-    const titlePTag = document.createElement("p");
-    titlePTag.appendChild(document.createTextNode(book.title));
-    bookContainer.id = index;
-    const authorPTag = document.createElement("p");
-    authorPTag.appendChild(document.createTextNode(book.author));
+  static bookList() {
+    const BookCollectionContainer = document.getElementById("books");
+    BookCollectionContainer.innerHTML = "";
 
-    const removeButtonTag = document.createElement("button");
-    removeButtonTag.appendChild(document.createTextNode("Remove"));
-    removeButtonTag.classList.add("delete-btn");
+    this.books = CollectionStorage.getFromStorage();
 
-    bookContainer.append(titlePTag, authorPTag, removeButtonTag);
-    BookCollectiContainer.appendChild(bookContainer);
-  });
+    this.books.map((book, index) => {
+      const bookContainer = document.createElement("li");
+      const titlePTag = document.createElement("p");
+      titlePTag.appendChild(document.createTextNode(book.title));
+      bookContainer.id = index;
+      const authorPTag = document.createElement("p");
+      authorPTag.appendChild(document.createTextNode(book.author));
+  
+      const removeButtonTag = document.createElement("button");
+      removeButtonTag.appendChild(document.createTextNode("Remove"));
+      removeButtonTag.classList.add("delete-btn");
+  
+      bookContainer.append(titlePTag, authorPTag, removeButtonTag);
+      BookCollectiContainer.appendChild(bookContainer);
+    });
+  }
 }
 
 
 const displayContainer = document.querySelector("#books");
 displayContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-btn")) {
-    BookCollecti.removeBook(e.target.parentNode.id);
+    BookCollection.removeBook(e.target.parentNode.id);
   }
-  BookCollecti();
+  BookCollection.bookList();
 });
