724da37e71645405872ded51333be479af00c065
Alef Garrido <oa.garrido11@gmail.com>
2021-08-10 16:50:46 -0500

    Fix remove method inside BookCollection class

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable array-callback-return */
+/* eslint-disable max-classes-per-file */
 
 // let books = [];
 
@@ -10,20 +11,19 @@ class Book {
   }
 }
 
-// Class to Handle Storage 
+// Class to Handle Storage
 
 class CollectionStorage {
-
   static saveToStorage(arr) {
-    localStorage.setItem("BookCollection", JSON.stringify(arr));
+    localStorage.setItem('BookCollection', JSON.stringify(arr));
   }
 
   static getFromStorage() {
-    let books
-    if (localStorage.getItem("BookCollection") === null) {
+    let books;
+    if (localStorage.getItem('BookCollection') === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("BookCollection"));
+      books = JSON.parse(localStorage.getItem('BookCollection'));
     }
     return books;
   }
@@ -31,15 +31,14 @@ class CollectionStorage {
 
 // Class of Book List
 class BookCollection {
-
   books = []
- 
+
   static addNewBook() {
-    const title = document.getElementById("bookTitle").value;
-    const author = document.getElementById("bookAuthor").value;
-  
+    const title = document.getElementById('bookTitle').value;
+    const author = document.getElementById('bookAuthor').value;
+
     const newBook = new Book(title, author);
-  
+
     this.books.push(newBook);
     CollectionStorage.saveToStorage(this.books);
     this.bookList();
@@ -47,36 +46,36 @@ class BookCollection {
 
   static removeBook(id) {
     this.books.splice(id, 1);
+    CollectionStorage.saveToStorage(this.books);
   }
 
   static bookList() {
-    const BookCollectionContainer = document.getElementById("books");
-    BookCollectionContainer.innerHTML = "";
+    const BookCollectionContainer = document.getElementById('books');
+    BookCollectionContainer.innerHTML = '';
 
     this.books = CollectionStorage.getFromStorage();
 
     this.books.map((book, index) => {
-      const bookContainer = document.createElement("li");
-      const titlePTag = document.createElement("p");
+      const bookContainer = document.createElement('li');
+      const titlePTag = document.createElement('p');
       titlePTag.appendChild(document.createTextNode(book.title));
       bookContainer.id = index;
-      const authorPTag = document.createElement("p");
+      const authorPTag = document.createElement('p');
       authorPTag.appendChild(document.createTextNode(book.author));
-  
-      const removeButtonTag = document.createElement("button");
-      removeButtonTag.appendChild(document.createTextNode("Remove"));
-      removeButtonTag.classList.add("delete-btn");
-  
+
+      const removeButtonTag = document.createElement('button');
+      removeButtonTag.appendChild(document.createTextNode('Remove'));
+      removeButtonTag.classList.add('delete-btn');
+
       bookContainer.append(titlePTag, authorPTag, removeButtonTag);
       BookCollectionContainer.appendChild(bookContainer);
     });
   }
 }
 
-
-const displayContainer = document.querySelector("#books");
-displayContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete-btn")) {
+const displayContainer = document.querySelector('#books');
+displayContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete-btn')) {
     BookCollection.removeBook(e.target.parentNode.id);
   }
   BookCollection.bookList();
