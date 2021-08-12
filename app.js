b6d75169fa8eeec8042b3c2d3e9b3c1c7c3ab315
Maha Magdy <mahamagdy3@gmail.com>
2021-08-12 21:56:31 +0200

    Add the nav, and its functionality

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ class Book {
 
 class CollectionStorage {
   static saveToStorage(arr) {
-    localStorage.setItem('BookCollection', JSON.stringify(arr));
+    localStorage.setItem("BookCollection", JSON.stringify(arr));
   }
 
   static getFromStorage() {
     let books;
-    if (localStorage.getItem('BookCollection') === null) {
+    if (localStorage.getItem("BookCollection") === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem('BookCollection'));
+      books = JSON.parse(localStorage.getItem("BookCollection"));
     }
     return books;
   }
@@ -29,11 +29,11 @@ class CollectionStorage {
 // Class of Book List
 
 class BookCollection {
-  books = []
+  books = [];
 
   static addNewBook() {
-    const title = document.getElementById('bookTitle').value;
-    const author = document.getElementById('bookAuthor').value;
+    const title = document.getElementById("bookTitle").value;
+    const author = document.getElementById("bookAuthor").value;
 
     const newBook = new Book(title, author);
 
@@ -48,29 +48,37 @@ class BookCollection {
   }
 
   static bookList() {
-    const BookCollectionContainer = document.getElementById('books');
-    BookCollectionContainer.innerHTML = '';
+    const BookCollectionContainer = document.getElementById("books");
+    BookCollectionContainer.innerHTML = "";
 
     this.books = CollectionStorage.getFromStorage();
 
     if (this.books.length === 0) {
-      const emptyState = document.createElement('p');
-      emptyState.innerHTML = 'Your bookshelf is empty :( <br /> Add some titles!';
-      emptyState.classList.add('text-center', 'p-3');
-      emptyState.classList.add('empty')
+      const emptyState = document.createElement("p");
+      emptyState.innerHTML =
+        "Your bookshelf is empty :( <br /> Add some titles!";
+      emptyState.classList.add("text-center", "p-3");
+      emptyState.classList.add("empty");
       BookCollectionContainer.append(emptyState);
     } else {
       this.books.map((book, index) => {
-        const bookContainer = document.createElement('li');
-        const titlePTag = document.createElement('p');
-        titlePTag.appendChild(document.createTextNode(`"${book.title}" by ${book.author}`));
-        titlePTag.classList.add('ms-5');
+        const bookContainer = document.createElement("li");
+        const titlePTag = document.createElement("p");
+        titlePTag.appendChild(
+          document.createTextNode(`"${book.title}" by ${book.author}`)
+        );
+        titlePTag.classList.add("ms-5");
         bookContainer.id = index;
-        bookContainer.classList.add('d-flex', 'justify-content-between');
+        bookContainer.classList.add("d-flex", "justify-content-between");
 
-        const removeButtonTag = document.createElement('button');
-        removeButtonTag.appendChild(document.createTextNode('Remove'));
-        removeButtonTag.classList.add('delete-btn', 'me-5', 'btn', 'btn-secondary');
+        const removeButtonTag = document.createElement("button");
+        removeButtonTag.appendChild(document.createTextNode("Remove"));
+        removeButtonTag.classList.add(
+          "delete-btn",
+          "me-5",
+          "btn",
+          "btn-secondary"
+        );
 
         bookContainer.append(titlePTag, removeButtonTag);
         BookCollectionContainer.appendChild(bookContainer);
@@ -79,10 +87,39 @@ class BookCollection {
   }
 }
 
-const displayContainer = document.querySelector('#books');
-displayContainer.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete-btn')) {
+const displayContainer = document.querySelector("#books");
+displayContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
     BookCollection.removeBook(e.target.parentNode.id);
   }
   BookCollection.bookList();
+});
+
+// navbar manipulation
+const listOption = document.getElementById("list-option");
+const addNewOption = document.getElementById("add-new-option");
+const contactOption = document.getElementById("contact-option");
+const listSection = document.getElementById("list-sec");
+const addNewBookSection = document.getElementById("add-new-book-sec");
+const contactSection = document.getElementById("contact-sec");
+
+listOption.addEventListener("click", function (e) {
+  e.target.classList.toggle = "active";
+  listSection.style.display = "block";
+  addNewBookSection.style.display = "none";
+  contactSection.style.display = "none";
+});
+
+addNewOption.addEventListener("click", function (e) {
+  e.target.classList.toggle = "active";
+  listSection.style.display = "none";
+  addNewBookSection.style.display = "block";
+  contactSection.style.display = "none";
+});
+
+contactOption.addEventListener("click", function (e) {
+  e.target.classList.toggle = "active";
+  listSection.style.display = "none";
+  addNewBookSection.style.display = "none";
+  contactSection.style.display = "block";
 });
